9239873c972d598ac496e70768955edc5d468af5
galileu.kim <galileukim@gmail.com>
2020-11-24 11:00:58 -0500

    validate cfscores with party

--- scripts/validate_cfscore.R
+++ scripts/validate_cfscore.R
@@ -1,5 +1,6 @@
 # input: cfscore estimates (baseline and robustness)
 # output: validation measures for cfscore estimates
+# also generate some intuition regarding the distribution of policy preferences
 library(tidyverse)
 library(data.table)
 library(here)
@@ -69,6 +70,26 @@ cfscore_fed_robust <- map_dfr(
   .id = "percentile"
 )
 
+# demonstrate intuition of the cfscores
+candidate_cpf <- list.files(
+  here("data/candidate/fed_state"),
+  pattern = "^candidate",
+  full.names = TRUE
+) %>%
+  map_dfr(
+    fread,
+    integer64 = "character"
+  )
+
+# join with ideology scores
+candidate_cfscore <- candidate_cpf %>%
+  left_join(
+    cfscore_fed,
+    by = c("cpf_candidate")
+  )
+
+# ---------------------------------------------------------------------------- #
+# generate visualization for robustness scores
 cfscore_fed_validation <- cfscore_fed %>%
   left_join(
     cfscore_fed_robust,
@@ -102,22 +123,38 @@ cfscore_fed_validation %>%
   )
 
 # ---------------------------------------------------------------------------- #
-# demonstrate intuition of the cfscores
-candidate_cpf <- list.files(
-  here("data/candidate/fed_state"),
-  pattern = "^candidate",
-  full.names = TRUE
-) %>%
+election <- list.files(
+  here("data/election/"),
+  "^election",
+  full.names = T
+) %>% 
   map_dfr(
-    fread,
-    integer64 = "character"
+    . %>% 
+      fread(., integer64 = "character")
+  ) %>%
+  filter(
+    str_detect(candidate_status, "^deferido")
   )
 
-# join with ideology scores
-candidate_cfscore <- candidate_cpf %>%
-  left_join(
-    cfscore_fed,
-    by = c("cpf_candidate")
+# votes by candidate
+candidate_vote <- election %>% 
+  group_by(
+    cpf_candidate,
+    election_year
+  ) %>% 
+  summarise(
+    vote = sum(vote, na.rm = T)
+  ) %>%
+  ungroup()
+
+candidate_cfscore %>%
+  ggplot() +
+  geom_boxplot(
+    aes(x = election_year, y = cfscore_original, group = election_year),
+    outlier.shape = NA
+  ) +
+  coord_cartesian(
+    ylim = c(-0.5, 0.1)
   )
 
 candidate_cfscore %>% 
@@ -134,7 +171,7 @@ candidate_cfscore %>%
   ) +
   geom_point(
     data = . %>% 
-      filter(!highlight),
+      filter(highlight),
     aes(
       cfscore_original,
       0
@@ -143,25 +180,106 @@ candidate_cfscore %>%
     alpha = 0.4,
     position = position_jitter(width = 0, height = 0.005)
   ) +
-  # geom_text(
-  #   data = . %>% 
-  #     filter(highlight),
-  #   aes(
-  #     ideology,
-  #     0,
-  #     label = party,
-  #     size = party_share
-  #   ),
-  #   col = "brown3",
-  #   alpha = 0.9,
-  #   position = position_jitter(width = 0, height = 0.005)
-  # ) +
+  geom_text(
+    data = . %>% 
+      filter(highlight),
+    aes(
+      cfscore_original,
+      0,
+      label = party
+    ),
+    col = "brown3",
+    alpha = 0.9,
+    position = position_jitter(width = 0, height = 0.005)
+  ) +
   scale_size(
     range = c(1, 12)
   ) +
-  facet_grid(election_year ~ elected) +
+  facet_grid(election_year ~ .) +
   coord_cartesian(
     ylim = c(-0.025, 0.025),
     xlim = c(-0.5, 0.2)
   ) +
   theme_minimal()
+
+# ideology
+party_ideology_candidate <- candidate_cfscore %>%
+  filter(
+    election_year > 2002
+  ) %>% 
+  left_join(
+    candidate_vote,
+    by = c("cpf_candidate", "election_year")
+  ) %>% 
+  mutate(
+    highlight = if_else(
+      party %in% c("pt", "pmdb", "psdb"), T, F
+    )
+  ) %>% 
+  group_by(
+    election_year
+  ) %>% 
+  mutate(
+    total_vote = sum(vote, na.rm = T)
+  ) %>% 
+  group_by(
+    party,
+    highlight,
+    add = T
+  ) %>% 
+  summarise(
+    ideology = mean(cfscore_original, na.rm = T),
+    party_share = sum(vote, na.rm = T)/unique(total_vote)
+  )
+
+party_ideology_candidate %>% 
+  ggplot() +
+  geom_hline(
+    yintercept = 0,
+    col = "grey35",
+    linetype = "dashed"
+  ) +
+  geom_point(
+    data = . %>% 
+      filter(!highlight),
+    aes(
+      ideology,
+      0,
+      size = party_share
+    ),
+    col = "darkorange",
+    alpha = 0.4,
+    position = position_jitter(width = 0, height = 0.005)
+  ) +
+  geom_text(
+    data = . %>% 
+      filter(highlight),
+    aes(
+      ideology,
+      0,
+      label = party,
+      size = party_share
+    ),
+    col = "brown3",
+    alpha = 0.9,
+    position = position_jitter(width = 0, height = 0.005)
+  ) +
+  scale_size(
+    range = c(1, 12)
+  ) +
+  facet_grid(election_year ~ .) +
+  coord_cartesian(
+    ylim = c(-0.025, 0.025),
+    xlim = c(-0.5, 0.2)
+  ) +
+  theme(
+    legend.position = "none",
+    axis.line.y = element_blank(),
+    axis.ticks.y = element_blank(),
+    axis.title.y = element_blank(),
+    axis.text.y = element_blank(),
+    panel.grid = element_blank()
+  ) 
+  # ggsave(
+  #   here("Presentation/figs/ideology/party_ideology_pelotas.pdf")
+  # )
