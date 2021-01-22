898ffdd0513bb4b96a265ff96ff40d450838882f
galileu.kim <galileukim@gmail.com>
2021-01-22 15:41:41 -0500

    fix joins

--- scripts/validate_unobserved_valence.R
+++ scripts/validate_unobserved_valence.R
@@ -4,6 +4,7 @@ library(here)
 corruption <- read_csv(
     here("data/corruption/corrupt_candidate_2018.csv")
 ) %>%
+    distinct(cpf_candidate, candidate_name) %>%
     transmute(
         cpf_candidate,
         candidate_name,
@@ -23,7 +24,8 @@ candidate_corruption <- candidate %>%
     mutate(
         female = if_else(gender == 4, 1, 0),
         # higher_edu = if_else(edu == "higher education", 1, 0),
-        edu = fct_relevel(edu, "lower school"),
+        edu = ifelse(edu == "middle school", "middle education", edu) %>%
+            fct_relevel("lower school"),
         corrupt = ifelse(is.na(corrupt), 0, corrupt),
         occupation = if_else(
             occupation %in% c("government", "business", "technician", "white-collar"),
@@ -40,6 +42,7 @@ logit <- glm(
 
 stargazer::stargazer(
     logit,
+    omit = "Constant",
     title = "Logistic Regression: Unobserved and Observed Valence Characteristics",
     dep.var.labels = "Corruption Indictment",
     covariate.labels = c(
